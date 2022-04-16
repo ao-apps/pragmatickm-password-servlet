412e7c9ea1997196200cc842c1ce4e5dbe405657
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:20 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/password/servlet/impl/PasswordTableImpl.java
+++ src/main/java/com/pragmatickm/password/servlet/impl/PasswordTableImpl.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-password-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.password.servlet.impl;
 
 import com.aoapps.html.any.AnyPalpableContent;
